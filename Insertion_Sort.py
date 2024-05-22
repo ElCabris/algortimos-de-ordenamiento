10842f497fb6bbf20051e155e6bed4ff21c51a00
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-05-22 01:13:01 -0500

    Insertion Sort

---
+++ Insertion_Sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(a):
+
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and key < a[j]:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+
+arreglo = [12, 11, 13, 5, 6]
+insertion_sort(arreglo)
+print(arreglo)
